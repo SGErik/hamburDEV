f5f87d011ca4e45d40ea0a11022a1f839896f411
Matheus Moura <matheusmouraa@icloud.com>
2022-09-03 15:00:39 -0300

    feat: Inserindo componente do Router.jsx para o App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Router from "./Router";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <Router />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
