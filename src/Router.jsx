8befa4eb0209d43a4833d4d3a5cd322d2fe13958
Matheus Moura <matheusmouraa@icloud.com>
2022-09-03 17:25:01 -0300

    feat: Inserindo Rota para página Home no Router

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FacaSeuPedido from "./pages/FacaSeuPedido/FacaSeuPedido";
+import Home from "./pages/Home/Home";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/facaseupedido" element={<FacaSeuPedido />} />
       </Routes>
     </BrowserRouter>
