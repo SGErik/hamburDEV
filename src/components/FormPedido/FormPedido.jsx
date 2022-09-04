53d8cda322b05720981062aab85dca1bfcc29bee
Matheus Moura <matheusmouraa@icloud.com>
2022-09-03 22:56:38 -0300

    feat: finalizando método POST do CRUD

--- src/components/FormPedido/FormPedido.jsx
+++ src/components/FormPedido/FormPedido.jsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import S from "../FormPedido/FormPedido.module.css";
 
 const FormPedido = () => {
+  const numeroDoPedido = Math.floor(Math.random() * 100).toString();
   const [dataForm, setDataForm] = useState({
-    pedido: "",
+    pedido: numeroDoPedido,
     nome: "",
     endereco: "",
     itensdopedido: "",
@@ -12,8 +13,19 @@ const FormPedido = () => {
     total: "",
   });
 
+  function ValidaProdutoPreco() {
+    if (dataForm.itensdopedido === "X-Salada") {
+      dataForm.total = "R$16,00";
+    } else return;
+  }
+
   function handleChange(e, nomeDaChave) {
     setDataForm({ ...dataForm, [nomeDaChave]: e.target.value });
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    ValidaProdutoPreco();
     console.log(dataForm);
   }
 
@@ -23,8 +35,18 @@ const FormPedido = () => {
         <h1>Efetue seu pedido</h1>
         <fieldset>
           <label htmlFor="produto">Escolha seu produto:</label>
-          <select name="" id="produto">
-            <option value="">X-Salada</option>
+          <select
+            name=""
+            id="produto"
+            value={dataForm.itensdopedido}
+            onChange={(e) => handleChange(e, "itensdopedido")}
+          >
+            <option value="">Selecione um produto</option>
+            <option>X-Salada</option>
+            <option>X-Bacon</option>
+            <option>X-Bacon Salada</option>
+            <option>X-Calabresa</option>
+            <option>X-Tudo</option>
           </select>
         </fieldset>
         <fieldset>
@@ -37,24 +59,40 @@ const FormPedido = () => {
         </fieldset>
         <fieldset>
           <label htmlFor="endereco">Endereço de Entrega</label>
-          <input type="text" id="endereco" />
+          <input
+            onChange={(e) => handleChange(e, "endereco")}
+            type="text"
+            id="endereco"
+          />
         </fieldset>
         <fieldset>
           <label htmlFor="pagamento">Método de pagamento</label>
-          <select name="" id="pagamento">
-            <option value="">Cartāo de Crédito</option>
-            <option value="">PIX</option>
-            <option value="">Dinheiro</option>
+          <select
+            name=""
+            id="pagamento"
+            value={dataForm.formadepagamento}
+            onChange={(e) => handleChange(e, "formadepagamento")}
+          >
+            <option value="">Selecione a forma que deseja pagar</option>
+            <option>Cartāo de Crédito</option>
+            <option>PIX</option>
+            <option>Dinheiro</option>
           </select>
         </fieldset>
         <fieldset>
-          <label htmlFor="entrega">Método de pagamento</label>
-          <select name="" id="entrega">
-            <option value="">Receber em Casa</option>
-            <option value="">Retirar</option>
+          <label htmlFor="entrega">Entrega</label>
+          <select
+            name=""
+            id="entrega"
+            value={dataForm.mododeentrega}
+            onChange={(e) => handleChange(e, "mododeentrega")}
+          >
+            <option value="">Selecione o método de entrega</option>
+            <option>Receber em Casa</option>
+            <option>Retirar</option>
           </select>
         </fieldset>
-        <button>Efetuar Pedido</button>
+        <button onClick={handleClick}>Efetuar Pedido</button>
       </form>
     </div>
   );
