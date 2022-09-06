b9ce8d017092dd1baf544ffc4dd32fb4eee796a9
Matheus Moura <matheusmouraa@icloud.com>
2022-09-06 20:40:15 -0300

    feat: ajustando Router na branch que possuia erro

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MeusPedidos from "./components/MeusPedidos/MeusPedidos";
+import { BrowserRouter, Route, Routes as Switcher } from "react-router-dom";
+import Header from "./components/Header/Header";
+import AlterarPedido from "./pages/AlterarPedido/AlterarPedido";
 import FacaSeuPedido from "./pages/FacaSeuPedido/FacaSeuPedido";
 import Home from "./pages/Home/Home";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
+      <Header />
+      <Switcher>
         <Route path="/" element={<Home />} />
         <Route path="/facaseupedido" element={<FacaSeuPedido />} />
-        <Route path="/meuspedidos" element={<MeusPedidos />} />
-      </Routes>
+        <Route path="/alterarseupedido/:pedido" element={<AlterarPedido />} />
+      </Switcher>
     </BrowserRouter>
   );
 };
