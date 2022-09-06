717849a1e8181646009d787d23903c0d461120ef
Matheus-Hen <matheushlsilva2000@gmail.com>
2022-09-06 16:51:55 -0300

    PUT finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
 import "./App.css";
+import { PedidoContextProvider } from "./context/pedidoContext";
 import Router from "./Router";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <Router />
-    </div>
+    <PedidoContextProvider>
+      <div className="App">
+        <Router />
+      </div>
+    </PedidoContextProvider>
   );
 }
 
--- src/Router.jsx
+++ src/Router.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import AlterarPedido from "./pages/AlterarPedido/AlterarPedido";
 import FacaSeuPedido from "./pages/FacaSeuPedido/FacaSeuPedido";
 import Home from "./pages/Home/Home";
 
 const Router = () => {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/facaseupedido" element={<FacaSeuPedido />} />
+        <Route path="/alterarseupedido/:pedido" element={<AlterarPedido />} />
       </Routes>
     </BrowserRouter>
   );
