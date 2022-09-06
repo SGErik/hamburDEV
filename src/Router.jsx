a1c48b95d0f5561165a751458a40b84d6a1bf60d
Matheus Moura <matheusmouraa@icloud.com>
2022-09-06 19:53:15 -0300

    feat: criacao do componente GET para listar pedidos

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MeusPedidos from "./components/MeusPedidos/MeusPedidos";
 import FacaSeuPedido from "./pages/FacaSeuPedido/FacaSeuPedido";
 import Home from "./pages/Home/Home";
 
@@ -9,6 +10,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/facaseupedido" element={<FacaSeuPedido />} />
+        <Route path="/meuspedidos" element={<MeusPedidos />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/MeusPedidos/MeusPedidos.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import S from "../MeusPedidos/MeusPedidos.module.css";
+
+const MeusPedidos = () => {
+  return (
+    <div className={S.primaryContainer}>
+      <h1>Meus pedidos</h1>
+    </div>
+  );
+};
+
+export default MeusPedidos;
