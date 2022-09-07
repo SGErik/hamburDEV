a4a44f9fafda9489091299da7a2abb040ea68067
Matheus Moura <matheusmouraa@icloud.com>
2022-09-07 00:47:13 -0300

    feat: Listando os pedidos na tela

--- src/components/MeusPedidos/MeusPedidos.jsx
+++ src/components/MeusPedidos/MeusPedidos.jsx
@@ -1,34 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import S from "../MeusPedidos/MeusPedidos.module.css";
 import { TbEdit, TbTrash } from "react-icons/tb";
+import { api } from "../../Services/api";
 
 const MeusPedidos = () => {
+  const [dados, setDados] = useState([]);
+
+  useEffect(() => {
+    function dataGet() {
+      try {
+        api.get("/pedidos").then((response) => {
+          setDados(response.data);
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    dataGet();
+    console.log(dados);
+  }, []);
+
   return (
     <div className={S.primaryContainer}>
-      <h1>Meus pedidos</h1>
-      <div className={S.cardProducts}>
-        <div className={S.cardInfo}>
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE1TPTOkAgcOOL7HcHKAlj_LniimOiedHZdQ&usqp=CAU"
-            alt=""
-          />
-          <div className={S.cardData}>
-            <h1>
-              <span>#51</span> Joao Silva
-            </h1>
-            <p>Produto: X-Salada</p>
-            <p>Entrega: Delivery</p>
-            <p>Endereço: Delivery</p>
-            <h1>R$17,00</h1>
+      <h1 className={S.Title}>Meus pedidos</h1>
+      {dados.map((dado) => {
+        return (
+          <div className={S.cardProducts} key={dado._id}>
+            <div className={S.cardInfo}>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE1TPTOkAgcOOL7HcHKAlj_LniimOiedHZdQ&usqp=CAU"
+                alt=""
+              />
+              <div className={S.cardData}>
+                <h1>
+                  <span>#{dado.pedido}</span>
+                  {dado.nome}
+                </h1>
+                <div className={S.InfoProducts}>
+                  <p>Produto: {dado.itensdopedido}</p>
+                  <p>Entrega: {dado.endereco}</p>
+                  <p>Endereço: {dado.mododeentrega}</p>
+                </div>
+                <div className="PaymentData">
+                  <h4>{dado.total}</h4>
+                  <p>{dado.formadepagamento}</p>
+                </div>
+              </div>
+              <div className="btnActions">
+                <div>
+                  <TbEdit size={28} color="black" />
+                </div>
+                <div>
+                  <TbTrash size={28} color="black" />
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="btnActions">
-            <h1>
-              <TbEdit size={32} color="black" />
-              <TbTrash size={32} color="black" />
-            </h1>
-          </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 };
