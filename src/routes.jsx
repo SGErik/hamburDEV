841ead0c8b39efd5197d6b51698086e28c3f320a
Matheus Moura <matheusmouraa@icloud.com>
2022-09-08 18:39:46 -0300

    feat: Removendo componente incorreto

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route, Routes as Switcher } from "react-router-dom";
 import Header from "./components/Header/Header";
+import MeusPedidos from "./components/MeusPedidos/MeusPedidos";
 import AlterarPedido from "./pages/AlterarPedido/AlterarPedido";
 import FacaSeuPedido from "./pages/FacaSeuPedido/FacaSeuPedido";
 import Home from "./pages/Home/Home";
-import Pedidos from "./pages/Pedidos/Pedidos";
 
 const Router = () => {
   return (
@@ -14,7 +14,7 @@ const Router = () => {
         <Route path="/" element={<Home />} />
         <Route path="/facaseupedido" element={<FacaSeuPedido />} />
         <Route path="/alterarseupedido/:pedido" element={<AlterarPedido />} />
-        <Route path="/meuspedidos" element={<Pedidos />} />
+        <Route path="/meuspedidos" element={<MeusPedidos />} />
       </Switcher>
     </BrowserRouter>
   );
