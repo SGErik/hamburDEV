22141e22749007f77f13a120742fa2334c76ed5a
Matheus Moura <matheusmouraa@icloud.com>
2022-09-07 21:38:42 -0300

    feat: Listando e estilizando os Meus pedidos

--- src/components/MeusPedidos/MeusPedidos.jsx
+++ src/components/MeusPedidos/MeusPedidos.jsx
@@ -23,41 +23,43 @@ const MeusPedidos = () => {
   return (
     <div className={S.primaryContainer}>
       <h1 className={S.Title}>Meus pedidos</h1>
-      {dados.map((dado) => {
-        return (
-          <div className={S.cardProducts} key={dado._id}>
-            <div className={S.cardInfo}>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE1TPTOkAgcOOL7HcHKAlj_LniimOiedHZdQ&usqp=CAU"
-                alt=""
-              />
-              <div className={S.cardData}>
-                <h1>
-                  <span>#{dado.pedido}</span>
-                  {dado.nome}
-                </h1>
-                <div className={S.InfoProducts}>
-                  <p>Produto: {dado.itensdopedido}</p>
-                  <p>Entrega: {dado.endereco}</p>
-                  <p>Endereço: {dado.mododeentrega}</p>
+      {dados
+        .map((dado) => {
+          return (
+            <div className={S.cardProducts} key={dado._id}>
+              <div className={S.cardInfo}>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE1TPTOkAgcOOL7HcHKAlj_LniimOiedHZdQ&usqp=CAU"
+                  alt=""
+                />
+                <div className={S.cardData}>
+                  <h1>
+                    <span>#{dado.pedido}</span>
+                    {dado.nome}
+                  </h1>
+                  <div className={S.InfoProducts}>
+                    <p>Produto: {dado.itensdopedido}</p>
+                    <p>Entrega: {dado.endereco}</p>
+                    <p>Endereço: {dado.mododeentrega}</p>
+                  </div>
+                  <div className="PaymentData">
+                    <h4>{dado.total}</h4>
+                    <p>{dado.formadepagamento}</p>
+                  </div>
                 </div>
-                <div className="PaymentData">
-                  <h4>{dado.total}</h4>
-                  <p>{dado.formadepagamento}</p>
-                </div>
-              </div>
-              <div className="btnActions">
-                <div>
-                  <TbEdit size={28} color="black" />
-                </div>
-                <div>
-                  <TbTrash size={28} color="black" />
+                <div className="btnActions">
+                  <div>
+                    <TbEdit size={28} color="black" />
+                  </div>
+                  <div>
+                    <TbTrash size={28} color="black" />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+        .reverse()}
     </div>
   );
 };
