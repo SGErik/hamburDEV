4485a4c6e2b865ff4c8e110f4f12043239db3702
Matheus Moura <matheusmouraa@icloud.com>
2022-09-08 18:37:23 -0300

    fix: Removendo componente incorreto

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes as Switcher } from "react-router-dom";
 import Header from "./components/Header/Header";
-import MeusPedidos from "./components/MeusPedidos/MeusPedidos";
 import AlterarPedido from "./pages/AlterarPedido/AlterarPedido";
 import FacaSeuPedido from "./pages/FacaSeuPedido/FacaSeuPedido";
 import Home from "./pages/Home/Home";
