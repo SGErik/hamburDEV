c5641e865892d67619b7619618ec7759aed707c6
Matheus-Hen <matheushlsilva2000@gmail.com>
2022-09-09 16:26:56 -0300

    fix: Delete ajustado

--- src/components/MeusPedidos/MeusPedidos.jsx
+++ src/components/MeusPedidos/MeusPedidos.jsx
@@ -6,28 +6,31 @@ import { PedidoContext } from "../../context/pedidoContext";
 import { useNavigate } from "react-router-dom";
 
 const MeusPedidos = () => {
-  const {pedido, setPedido} = useContext(PedidoContext)
+  const { pedido, setPedido } = useContext(PedidoContext)
   const [dados, setDados] = useState([]);
-  const [deleta, setDeleta] = useState(false)
+  const [Index, setIndex] = useState(-2)
   const navigate = useNavigate()
   useEffect(() => {
-    dataGet(setDados,dados);
+    dataGet(setDados, dados);
   }, []);
-  useEffect(()=>{
-    if(deleta == true){
+  useEffect(() => {
+    if (Index != -2) {
       dataDelete(pedido)
-      dataGet(setDados,dados)
-      setDeleta(false)
+      setTimeout(() => {
+        
+        console.log("entrei");
+        document.location.reload(true)
+      }, 1000);
     }
-  },[deleta])
-  
+  }, [Index])
+
   return (
     <div className={S.primaryContainer}>
       <h1 className={S.Title}>Meus pedidos</h1>
       {dados
-        .map((dado) => {
+        .map((dado, index) => {
           return (
-            <div className={S.cardProducts} key={dado._id}>
+            <div className={S.cardProducts} key={index}>
               <div className={S.cardInfo}>
                 <img
                   src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE1TPTOkAgcOOL7HcHKAlj_LniimOiedHZdQ&usqp=CAU"
@@ -37,7 +40,7 @@ const MeusPedidos = () => {
                   <h1>
                     <span>#{dado.pedido}</span>
                     <br></br>
-                     {dado.nome}
+                    {dado.nome}
                   </h1>
                   <div className={S.InfoProducts}>
                     <p>Produto: {dado.itensdopedido}</p>
@@ -51,15 +54,15 @@ const MeusPedidos = () => {
                 </div>
                 <div className="btnActions">
                   <div>
-                    <TbEdit size={28} color="black" onClick={()=>{
-                          setPedido(dado.pedido)
-                          navigate(`/alterarseupedido/${pedido}`)
-                    }}/>
+                    <TbEdit size={28} color="black" cursor="pointer" onClick={() => {
+                      setPedido(dado.pedido)
+                      navigate(`/alterarseupedido/${pedido}`)
+                    }} />
                   </div>
                   <div>
-                    <TbTrash size={28} color="black" onClick={()=>{
-                       setPedido(dado.pedido)
-                       setDeleta(true)
+                    <TbTrash size={28} color="black" cursor="pointer" onClick={() => {
+                      setPedido(dado.pedido)
+                      setIndex(index)
                     }} />
                   </div>
                 </div>
