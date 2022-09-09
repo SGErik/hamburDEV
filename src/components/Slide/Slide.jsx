7240e4a831c9a3e089e654362b9c64b7155154fa
SGErik <erik.souzaxbr@gmail.com>
2022-09-09 16:47:29 -0300

    Feita home responsiva e novo link no header

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,8 +1,8 @@
 import { Swiper, SwiperSlide} from 'swiper/react'
 import { Navigation, Autoplay} from 'swiper'
 import XSalada from '../../assets/XSalada.jpg'
-import XBacon from '../../assets/XBacon.jpg'
 import XCheedar from '../../assets/XCheedar.jpg'
+import XCalabresa from '../../assets/XCalabresa.jpg'
 import S from './Slide.module.css'
 import 'swiper/css/navigation'
 import 'swiper/css';
@@ -23,7 +23,7 @@ const Slide = () => {
     className={S.carrousel}
     >
     <SwiperSlide><img src={XSalada} /></SwiperSlide>
-    <SwiperSlide><img src={XBacon} /></SwiperSlide>
+    <SwiperSlide><img src={XCalabresa} /></SwiperSlide>
     <SwiperSlide><img src={XCheedar}  /></SwiperSlide>
 
     </Swiper>
