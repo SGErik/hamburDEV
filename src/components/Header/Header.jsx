af9284182b7820987a373bca4f820aeef4d6b7f8
SGErik <erik.souzaxbr@gmail.com>
2022-09-09 23:47:10 -0300

    Corrigido bug de redirecionamento no header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,8 +19,8 @@ const Header = () => {
                 </Link>
                 <div className={S.toPages}>
                     <NavLink to='/'>HOME</NavLink>
-                    <a href='#cardapio'>CARDÁPIO</a>
-                    <a href='#quemsomos'>QUEM SOMOS</a>
+                    <a href='/#cardapio'>CARDÁPIO</a>
+                    <a href='/#quemsomos'>QUEM SOMOS</a>
                     <NavLink to='/meuspedidos'>MEUS PEDIDOS</NavLink>
                     <Link to='/facaseupedido'>
                         <Button className={S.btn} text='DELIVERY' />
